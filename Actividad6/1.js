dda03e67c27d95aa45b6a501dcb74b58f020e0c3
gabriel-solis <gasoa-xbox@hotmail.com>
2022-02-01 19:48:22 -0600

    Actividad 6

---
+++ Actividad6/1.js
@@ -0,0 +1,19 @@
+//1 concatenar los siguientes arreglos
+var animals = ["eagle", "parrot", "monkey", "boar", "lion"]
+var comingSoonAnimal = ["panter", "dragon", "turtle"]
+ 
+var arrAnimal = animals.concat(comingSoonAnimal)
+
+console.log(arrAnimal)
+
+//2 acomoda el arreglo de menor a mayor
+var arr = [4,6,1,0,8,2]
+
+arr.sort()
+console.log(arr)
+//3 agrega un nuevo animal ("cow") al arreglo de animals
+animals.push("cow")
+console.log(animals)
+//4 retira el elemento "eagle" del arreglo de animals
+animals.splice(0,1)
+console.log(animals)
